7a80d24f0d31da724b508ac830619eab3997ba2d
dolyguarana2007 <128715869+dolyguarana2007@users.noreply.github.com>
2024-05-28 08:46:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+const caixaPrincipal = document.querSelector("Caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoRessultados = document.querySelactor("rexto-resultado");
+
+const perguntas = [
+  {
+    enunciado:"quantos anos vc tem ?";
+    alternativa: [
+      "15 anos",
+        "16 anos",
+      ]
+},
+ {
+    enunciado:"oq vc gosta de fazer ?";
+    alternativa: [
+      "dormir",
+        "assistir serie",
+      ]
+},
+ {
+    enunciado:"vc tem algum animal de estimação?";
+    alternativa: [
+      "sim",
+        "não",
+      ]
+},
+ {
+    enunciado:"oq vc gosta de comer";
+    alternativa: [
+      "estrogonoff",
+        "macarrão",
+      ]
+},
+ {
+    enunciado:"vc tem celular?";
+    alternativa: [
+      "sim",
+        "não",
+      ]
+},
+
+
+
 
